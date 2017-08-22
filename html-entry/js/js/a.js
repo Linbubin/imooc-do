242ec799cb82072215e834232b423794948ff136
linbin <linbin@taodi.com>
2017-08-22 17:17:44 +0800

    通过按钮来跳转img

--- html-entry/js/js/a.js
+++ html-entry/js/js/a.js
@@ -2,7 +2,8 @@ var index = 0,
 		divImg = getById('banner').getElementsByTagName('div'),
 		len = divImg.length-1,
 		prevBtn = getById('prev'),
-		nextBtn = getById('next');
+		nextBtn = getById('next'),
+		dotsImg = getById('dots').getElementsByTagName('span');
 
 function getById(id){
 	return document.getElementById(id);
@@ -11,11 +12,13 @@ function getById(id){
 function toPrevImg(){
 	index = index == 0 ? len : index-1;
 	changeImg();
+	changeDots();
 }
 
 function toNextImg(){
-	index = index == len ? 1 : index+1;
+	index = index == len ? 0 : index+1;
 	changeImg();
+	changeDots();
 }
 
 function changeImg(){
@@ -25,12 +28,32 @@ function changeImg(){
 	 divImg[index].style.display = 'block';
 }
 
+function changeDots(){
+	for(var i=0; i<=len;i++){
+		dotsImg[i].className = '';
+	}
+	dotsImg[index].className = 'active';
+}
+
+function changeDotsAndImg(){
+	changeImg();
+	changeDots();
+}
+
 function main(){
 	prevBtn.onclick=function(){
 		toPrevImg();
 	}
 	nextBtn.onclick=function(){
 		toNextImg();
+	}
+
+	for(var j=0;j<=len;j++){
+		dotsImg[j].id = j;
+		dotsImg[j].onclick = function(){
+			index = Number(this.id);
+			changeDotsAndImg();
+		}
 	}
 }
 main();
